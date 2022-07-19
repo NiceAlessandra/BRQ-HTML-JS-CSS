fbb37cf8f3927ca9b3de6462e519bb0212bbe8b5
Nice Alessandra <nicealessandra@gmail.com>
2022-07-19 19:39:17 -0300

    Correção método exerc. Fetch3

--- JavaScript/Fetch3/script.js
+++ JavaScript/Fetch3/script.js
@@ -3,11 +3,12 @@ let inputCEP = document.querySelector('#cep');
 let inputLogradouro = document.querySelector('#logradouro');
 let inputBairro = document.querySelector('#bairro');
 let inputLocalidade = document.querySelector('#localidade');
-let inputUf= document.querySelector('#uf');
+let inputUF= document.querySelector('#uf');
 //via cep
 
 const ConsultarCEP = (cep) => {
-    var endpoint = "http://viacep.com.br/ws/'<cep>'/json/";
+    
+    var endpoint = 'https://viacep.com.br/ws/'+cep+'/json/';
 
     fetch(endpoint, {
         method: 'GET',
@@ -19,19 +20,16 @@ const ConsultarCEP = (cep) => {
         inputLogradouro.value = result.logradouro;
         inputBairro.value = result.bairro;
         inputLocalidade.value = result.localidade;
-        inputUf.value = result.uf;
-        // parei aqui
+        inputUF.value = result.uf;
+        
 
     })
     .catch(erro => console.log(erro))
 
-    const ConsultarCEP = (cep) => {
-        var endpoint = "http://viacep.com.br/ws/12517540/json/";
-    }
 }
 
 inputCEP.addEventListener('input', () =>{
     if (inputCEP.value.length > 7 ){
-        console.log('Pode consultar');
+        ConsultarCEP(inputCEP.value);
     }
 })
